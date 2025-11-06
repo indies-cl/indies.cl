d56831feea79e38e6748cadfbd750f20335d3669
AlvareYN <alvaro21fuentes@gmail.com>
2025-11-05 22:11:07 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Indies</h1>
-    </div>
+    <main>
+      <div className="absolute -top-[110%] z-[-1] w-[100dvw]">
+        <Image
+          width={0}
+          height={0}
+          className="h-full w-full object-cover"
+          src="/background-elements.svg"
+          alt="xd"
+        />
+      </div>
+      <div className="absolute top-0 z-[-2] mx-auto h-full w-[100dvw]">
+        <Image
+          width={0}
+          height={0}
+          className="h-full w-full object-cover"
+          src="/bitmap-xl-svgo.svg"
+          alt="xd"
+        />
+      </div>
+      <section className="mx-auto flex max-w-[1280px]">
+        <h2>Welcome to Indies</h2>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <h2>Welcome to Indies</h2>
+      <p>Your gateway to indie content.</p>
+    </main>
   );
 }
